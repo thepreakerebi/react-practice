05f00f2bc9e040e5f615b5644f2d0318cee6a617
Elvis Kerebi <elviskerebi@Elviss-MacBook-Pro.local>
2024-08-17 14:56:39 +0200

    conditionally run effects

--- src/components/CounterOne.js
+++ src/components/CounterOne.js
@@ -3,13 +3,16 @@ import React, { useEffect, useState } from 'react'
 const CounterOne = () => {
 
     const [count, setCount] = useState(0);
+    const [name, setName] = useState('');
 
     useEffect(() => {
+        console.log('useEffect - updating document title');
         document.title = `Clicked ${count} times`;
-    })
+    }, [count])
 
   return (
     <div>
+      <input value={name} onChange={e => setName(e.target.value)} />
       <button onClick={() => setCount(count + 1)}>Clicked {count} times </button>
     </div>
   )
