04f9e9dc7f62604d5c22347e9a3444f07c1a724c
Elvis Kerebi <elviskerebi@Elviss-MacBook-Pro.local>
2024-08-19 11:03:43 +0200

    useReducer part 1

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
-import React from 'react';
-import ContextComponentC from "./components/ContextComponentC";
+import CounterReducer from "./components/CounterReducer";
 
-export const UserContext = React.createContext();
-export const ChannelContext = React.createContext();
 
 
 function App() {
   return (
     <div className="App">
-      <UserContext.Provider value={'John'}>
-        <ChannelContext.Provider value={'Doe'}>
-          <ContextComponentC />
-        </ChannelContext.Provider>
-      </UserContext.Provider>
+      <CounterReducer />
     </div>
   );
 }
