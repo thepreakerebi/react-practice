12e2d6a50a5debbe76e770d20c97995d412ea0b2
Elvis Kerebi <elviskerebi@Elviss-MacBook-Pro.local>
2024-08-21 11:57:06 +0200

    using useRef hook to access DOM nodes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import MemoCounter from "./components/MemoCounter";
+import InputRef from "./components/InputRef";
 
 
 
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <div className="App">
-      <MemoCounter />
+      <InputRef />
     </div>
     
   );
